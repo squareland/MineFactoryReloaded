2ea221739e8bb91e12ddcf611be0ddbd9229ad69
P3pp3rF1y <p3pp3rf1y@gmail.com>
2017-07-13 15:30:48 +0200

    Fixed porta spawner tooltip to display entities that spawner can spawn (and now can actually display multiple if the spawner is set to display more than one entity)

--- src/powercrystals/minefactoryreloaded/item/ItemPortaSpawner.java
+++ src/powercrystals/minefactoryreloaded/item/ItemPortaSpawner.java
@@ -5,16 +5,17 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.SoundCategory;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -26,6 +27,7 @@ import powercrystals.minefactoryreloaded.net.Packets;
 import powercrystals.minefactoryreloaded.render.ModelHelper;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemPortaSpawner extends ItemFactory {
@@ -33,6 +35,7 @@ public class ItemPortaSpawner extends ItemFactory {
 	private static Block _block = Blocks.MOB_SPAWNER;
 	public static final String spawnerTag = "spawner";
 	private static final String placeTag = "placeDelay";
+	private static final String entityNamesTag = "entityNames";
 
 	public ItemPortaSpawner() {
 
@@ -51,17 +54,9 @@ public class ItemPortaSpawner extends ItemFactory {
 		return null;
 	}
 
-	private static String getEntityId(@Nonnull ItemStack stack) {
-
-		NBTTagCompound tag = getSpawnerTag(stack);
-		if (tag != null)
-			return tag.getString("EntityId");
-		return null;
-	}
-
 	public static boolean hasData(@Nonnull ItemStack stack) {
 
-		return getEntityId(stack) != null;
+		return getSpawnerTag(stack) != null;
 	}
 
 	private static int getDelay(@Nonnull ItemStack stack) {
@@ -77,10 +72,15 @@ public class ItemPortaSpawner extends ItemFactory {
 	public void addInfo(@Nonnull ItemStack stack, EntityPlayer player, List<String> infoList, boolean advancedTooltips) {
 
 		super.addInfo(stack, player, infoList, advancedTooltips);
-		String id = getEntityId(stack);
-		if (id != null)
-			infoList.add(MFRUtil.localize("tile.mobSpawner") + ": " +
-					MFRUtil.localize("entity.", id));
+		if (hasData(stack)) {
+			String entities = "";
+			for (String entityUnlocalized : getEntityNames(stack)) {
+				if (entities.length() > 0)
+					entities += ", ";
+				entities += MFRUtil.localize("entity.", entityUnlocalized);
+			}
+			infoList.add(MFRUtil.localize("tile.mobSpawner") + ": " + entities);
+		}
 		int delay = getDelay(stack);
 		if (delay > 0) {
 			String s = MFRUtil.localize("tip.info.mfr.cannotplace", true, "%s");
@@ -99,7 +99,7 @@ public class ItemPortaSpawner extends ItemFactory {
 
 	@Override
 	public boolean shouldCauseReequipAnimation(@Nonnull ItemStack oldStack, @Nonnull ItemStack newStack, boolean slotChanged) {
-		
+
 		return !ItemHelper.areItemStacksEqualIgnoreTags(oldStack, newStack, placeTag);
 	}
 
@@ -112,13 +112,14 @@ public class ItemPortaSpawner extends ItemFactory {
 		if (world.isRemote) {
 			return EnumActionResult.SUCCESS;
 		}
-		if (getEntityId(itemstack) == null) {
+		if (!hasData(itemstack)) {
 			if (world.getBlockState(pos).getBlock().equals(_block)) {
 				TileEntity te = world.getTileEntity(pos);
 				NBTTagCompound tag = new NBTTagCompound();
 				tag.setTag(spawnerTag, new NBTTagCompound());
 				te.writeToNBT(tag.getCompoundTag(spawnerTag));
 				tag.setInteger(placeTag, 40 * 20);
+				tag.setTag(entityNamesTag, getEntityNames(tag));
 				itemstack.setTagCompound(tag);
 				world.setBlockToAir(pos);
 				return EnumActionResult.SUCCESS;
@@ -133,12 +134,49 @@ public class ItemPortaSpawner extends ItemFactory {
 		}
 	}
 
-	private boolean placeBlock(@Nonnull ItemStack itemstack, EntityPlayer player, EnumHand hand, World world, BlockPos pos, EnumFacing side,
+	private NBTTagList getEntityNames(NBTTagCompound tag) {
+
+		NBTTagList entityNames = new NBTTagList();
+
+		NBTTagCompound spawnerNBT = tag.getCompoundTag(spawnerTag);
+		NBTTagList potentialSpawns = spawnerNBT.getTagList("SpawnPotentials", 10);
+
+		for (int i = 0; i < potentialSpawns.tagCount(); i++) {
+			NBTTagCompound entityWeight = potentialSpawns.getCompoundTagAt(i);
+
+			if (!entityWeight.hasKey("Entity"))
+				continue;
+			NBTTagCompound entity = entityWeight.getCompoundTag("Entity");
+
+			String entityId = entity.getString("id");
+			if (!entityId.isEmpty())
+				entityNames.appendTag(new NBTTagString(EntityList.getTranslationName(new ResourceLocation(entityId))));
+		}
+		return entityNames;
+	}
+
+	private List<String> getEntityNames(ItemStack stack) {
+
+		List<String> entityNames = new ArrayList<>();
+		NBTTagCompound tag = stack.getTagCompound();
+
+		if (tag != null && tag.hasKey(entityNamesTag)) {
+			NBTTagList list = tag.getTagList(entityNamesTag, 8);
+			for(NBTBase name : list) {
+				entityNames.add(((NBTTagString)name).getString());
+			}
+		}
+
+		return entityNames;
+	}
+
+	private boolean placeBlock(@Nonnull ItemStack itemstack, EntityPlayer player, EnumHand hand, World world, BlockPos pos,
+			EnumFacing side,
 			float xOffset, float yOffset, float zOffset) {
 
 		IBlockState state = world.getBlockState(pos);
 		Block block = state.getBlock();
-		
+
 		BlockPos placePos = pos;
 
 		if (block == Blocks.SNOW_LAYER) {
@@ -154,12 +192,14 @@ public class ItemPortaSpawner extends ItemFactory {
 		} else if (placePos.getY() == 255 && state.getMaterial().isSolid()) {
 			return false;
 		} else if (world.mayPlace(_block, placePos, false, side, player)) {
-			IBlockState placedState = _block.getStateForPlacement(world, placePos, side, xOffset, yOffset, zOffset, 0, player, hand);
+			IBlockState placedState = _block
+					.getStateForPlacement(world, placePos, side, xOffset, yOffset, zOffset, 0, player, hand);
 
 			if (placeBlockAt(itemstack, player, world, placePos, side, xOffset, yOffset, zOffset, placedState)) {
 				SoundType soundType = block.getSoundType(state, world, placePos, null);
-				world.playSound(null, placePos.getX() + 0.5F, placePos.getY() + 0.5F, placePos.getZ() + 0.5F, soundType.getStepSound(), SoundCategory.BLOCKS,
-					(soundType.getVolume() + 1.0F) / 2.0F, soundType.getPitch() * 0.8F);
+				world.playSound(null, placePos.getX() + 0.5F, placePos.getY() + 0.5F, placePos.getZ() + 0.5F,
+						soundType.getStepSound(), SoundCategory.BLOCKS,
+						(soundType.getVolume() + 1.0F) / 2.0F, soundType.getPitch() * 0.8F);
 				itemstack.shrink(1);
 			}
 
@@ -169,7 +209,8 @@ public class ItemPortaSpawner extends ItemFactory {
 		}
 	}
 
-	private boolean placeBlockAt(@Nonnull ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, IBlockState state) {
+	private boolean placeBlockAt(@Nonnull ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side,
+			float hitX, float hitY, float hitZ, IBlockState state) {
 
 		// TODO: record and read the block that was consumed
 		if (!world.setBlockState(pos, state, 3)) {
