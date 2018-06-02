37b0008b822bd3efbe224dc48fdc274cba94b0e3
skyboy <mr.wraith.06@gmail.com>
2018-06-01 23:26:50 -0400

    Tweak zombie syringe

--- src/main/java/powercrystals/minefactoryreloaded/item/syringe/ItemSyringeZombie.java
+++ src/main/java/powercrystals/minefactoryreloaded/item/syringe/ItemSyringeZombie.java
@@ -2,11 +2,14 @@ package powercrystals.minefactoryreloaded.item.syringe;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityPigZombie;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.*;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -15,48 +18,46 @@ import powercrystals.minefactoryreloaded.setup.MFRThings;
 
 import javax.annotation.Nonnull;
 
-public class ItemSyringeZombie extends ItemSyringe
-{
-	public ItemSyringeZombie()
-	{
+public class ItemSyringeZombie extends ItemSyringe {
+
+	public ItemSyringeZombie() {
+
 		setUnlocalizedName("mfr.syringe.zombie");
 		setContainerItem(MFRThings.syringeEmptyItem);
 	}
 
 	@Override
-	public boolean canInject(World world, EntityLivingBase entity, @Nonnull ItemStack syringe)
-	{
-		return entity instanceof EntityAgeable && ((EntityAgeable)entity).getGrowingAge() < 0;
+	public boolean canInject(World world, EntityLivingBase entity, @Nonnull ItemStack syringe) {
+
+		return entity instanceof EntityAgeable && ((EntityAgeable) entity).getGrowingAge() < 0;
 	}
-	
+
 	@Override
-	public boolean inject(World world, EntityLivingBase entity, @Nonnull ItemStack syringe)
-	{
-		((EntityAgeable)entity).setGrowingAge(0);
-		if(world.rand.nextInt(100) < 5)
-		{
+	public boolean inject(World world, EntityLivingBase entity, @Nonnull ItemStack syringe) {
+
+		((EntityAgeable) entity).setGrowingAge(0);
+
+		if (world.rand.nextInt(100) < 5) {
 			Entity e = null;
-			if (entity instanceof EntityPig)
-			{
-				e = new EntityPigZombie(world);
-			}
-			else if (entity instanceof AbstractHorse)
-			{
-				if (entity instanceof EntityHorse) {
-					e = new EntityZombieHorse(world);
-					copyHorseAttributes((AbstractHorse) entity, (AbstractHorse) e);
-				} else if (entity instanceof EntityZombieHorse) {
-					e = new EntitySkeletonHorse(world);
-					copyHorseAttributes((AbstractHorse) entity, (AbstractHorse) e);
+			ResourceLocation entityType = EntityList.getKey(entity);
+
+			if (entityType == EntityList.getKey(EntityPig.class)) {
+				e = EntityList.newEntity(EntityPigZombie.class, world);
+			} else if (entity instanceof AbstractHorse) {
+				NBTTagCompound tag = entity.writeToNBT(new NBTTagCompound());
+				if (entityType == EntityList.getKey(EntityHorse.class)) {
+					e = EntityList.newEntity(EntityZombieHorse.class, world);
+				} else if (entityType == EntityList.getKey(EntityZombieHorse.class)) {
+					e = EntityList.newEntity(EntitySkeletonHorse.class, world);
 				}
+				if (e != null) {
+					e.readFromNBT(tag); // horse adds 'Variant' and 'ArmorItem' but should be empty from child
+				}
+			} else {
+				e = EntityList.newEntity(EntityZombie.class, world);
 			}
-			else
-			{
-				e = new EntityZombie(world);
-			}
-			
-			if (e != null)
-			{
+
+			if (e != null) {
 				e.setLocationAndAngles(entity.posX, entity.posY, entity.posZ, entity.rotationYaw, entity.rotationPitch);
 				world.spawnEntity(e);
 				entity.setDead();
@@ -65,18 +66,11 @@ public class ItemSyringeZombie extends ItemSyringe
 		return true;
 	}
 
-	private void copyHorseAttributes(AbstractHorse originalHorse, AbstractHorse newHorse) {
-
-		newHorse.copyLocationAndAnglesFrom(originalHorse);
-		newHorse.setHorseSaddled(originalHorse.isHorseSaddled());
-		newHorse.jumpPower = originalHorse.jumpPower;
-		newHorse.setTemper(originalHorse.getTemper());
-	}
-
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerModels() {
 
 		ModelHelper.registerModel(MFRThings.syringeZombieItem, "syringe", "variant=zombie");
 	}
+
 }
