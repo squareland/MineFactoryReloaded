25321fe427bc5134ddf23bcae7eeaec8cf3732fd
skyboy <mr.wraith.06@gmail.com>
2018-06-18 17:26:57 -0400

    Split main and api sources, build better jars

--- src/api/java/powercrystals/minefactoryreloaded/api/plant/IFactoryHarvestable.java
+++ src/api/java/powercrystals/minefactoryreloaded/api/plant/IFactoryHarvestable.java
@@ -1,13 +1,13 @@
 package powercrystals.minefactoryreloaded.api.plant;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Defines a harvestable block for the Harvester.
