5c341ecf4727cbd4aa91f5adb9d9a0d5b66ad6a3
skyboy <mr.wraith.06@gmail.com>
2018-06-18 14:06:24 -0400

    Fix an error in GrindableEnderman

--- src/main/java/powercrystals/minefactoryreloaded/farmables/grindables/GrindableEnderman.java
+++ src/main/java/powercrystals/minefactoryreloaded/farmables/grindables/GrindableEnderman.java
@@ -1,9 +1,5 @@
 package powercrystals.minefactoryreloaded.farmables.grindables;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -11,9 +7,12 @@ import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-
 import powercrystals.minefactoryreloaded.api.mob.IFactoryGrindable;
 import powercrystals.minefactoryreloaded.api.mob.MobDrop;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 public class GrindableEnderman implements IFactoryGrindable
 {
@@ -28,9 +27,7 @@ public class GrindableEnderman implements IFactoryGrindable
 	{
 		List<MobDrop> drops = new LinkedList<>();
 		IBlockState state = ((EntityEnderman)entity).getHeldBlockState();
-		Block block = null;
-		if (state != null)
-			state.getBlock();
+		Block block = state != null ? state.getBlock() : null;
 		if (block != null && !block.equals(Blocks.AIR))
 			drops.add(new MobDrop(10, new ItemStack(block, 1, block.damageDropped(state))));
 		return drops;
