d97a8928e85130cd8b8eb3240cd6da1e2031ac98
Radviger <radviger@gmail.com>
2021-06-13 15:44:59 +0200

    More GUI fixes.

--- src/main/java/powercrystals/minefactoryreloaded/gui/client/GUIBag.java
+++ src/main/java/powercrystals/minefactoryreloaded/gui/client/GUIBag.java
@@ -1,47 +1,36 @@
 package powercrystals.minefactoryreloaded.gui.client;
 
-import static cofh.core.init.CoreProps.PATH_GUI_STORAGE;
-
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-
-import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.util.ResourceLocation;
-
 import powercrystals.minefactoryreloaded.gui.container.ContainerBag;
 
-@SideOnly(Side.CLIENT)
-public class GUIBag extends GuiContainer
-{
-    private static final ResourceLocation guiTextures = new ResourceLocation(PATH_GUI_STORAGE + "storage_5.png");
-    private ContainerBag bag;
+import static cofh.core.init.CoreProps.PATH_GUI_STORAGE;
 
-    public GUIBag(ContainerBag container)
-    {
+@SideOnly(Side.CLIENT)
+public class GUIBag extends GuiContainer {
+    private static final ResourceLocation guiTextures = new ResourceLocation(PATH_GUI_STORAGE + "storage_5.png");
+    private final ContainerBag bag;
+
+    public GUIBag(ContainerBag container) {
         super(container);
         this.bag = container;
         this.allowUserInput = false;
         this.ySize = 148;
     }
 
-    /**
-     * Draw the foreground layer for the GuiContainer (everything in front of the items)
-     */
     @Override
-	protected void drawGuiContainerForegroundLayer(int par1, int par2)
-    {
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+        super.drawGuiContainerForegroundLayer(mouseX, mouseY);
         fontRenderer.drawString(this.bag.getInventoryName(), 8, 6, 4210752);
         fontRenderer.drawString(I18n.translateToLocal("container.inventory"), 8, this.ySize - 96 + 3, 4210752);
     }
 
-    /**
-     * Draw the background layer for the GuiContainer (everything behind the items)
-     */
     @Override
-	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
-    {
+    protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
         super.drawDefaultBackground();
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(guiTextures);
--- src/main/java/powercrystals/minefactoryreloaded/gui/client/GuiNeedlegun.java
+++ src/main/java/powercrystals/minefactoryreloaded/gui/client/GuiNeedlegun.java
@@ -10,33 +10,31 @@ import powercrystals.minefactoryreloaded.MFRProps;
 
 import javax.annotation.Nonnull;
 
-public class GuiNeedlegun extends GuiContainer
-{
-	private static final ResourceLocation needleGunGUI = new ResourceLocation(MFRProps.GUI_FOLDER + "needlegun.png");
-	private String name;
-	public GuiNeedlegun(Container container, @Nonnull ItemStack item)
-	{
-		super(container);
-		name = item.getDisplayName();
-	}
+public class GuiNeedlegun extends GuiContainer {
+    private static final ResourceLocation needleGunGUI = new ResourceLocation(MFRProps.GUI_FOLDER + "needlegun.png");
+    private String name;
 
-	@Override
-	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
-	{
-		fontRenderer.drawString(name, 8, 6, 4210752);
-		fontRenderer.drawString(I18n.translateToLocal("container.inventory"), 8, ySize - 96 + 4, 4210752);
+    public GuiNeedlegun(Container container, @Nonnull ItemStack item) {
+        super(container);
+        name = item.getDisplayName();
+    }
 
-		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-	}
+    @Override
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+        super.drawGuiContainerForegroundLayer(mouseX, mouseY);
+        fontRenderer.drawString(name, 8, 6, 4210752);
+        fontRenderer.drawString(I18n.translateToLocal("container.inventory"), 8, ySize - 96 + 4, 0xEFEFEF);
 
-	@Override
-	protected void drawGuiContainerBackgroundLayer(float gameTicks, int mouseX, int mouseY)
-	{
-		super.drawDefaultBackground();
-		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-		this.mc.renderEngine.bindTexture(needleGunGUI);
-		int x = (width - xSize) / 2;
-		int y = (height - ySize) / 2;
-		this.drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
-	}
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float gameTicks, int mouseX, int mouseY) {
+        super.drawDefaultBackground();
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        this.mc.renderEngine.bindTexture(needleGunGUI);
+        int x = (width - xSize) / 2;
+        int y = (height - ySize) / 2;
+        this.drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
+    }
 }
