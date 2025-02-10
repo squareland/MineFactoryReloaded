25227f73320c0029a90df7bc666cd430da8b7026
vkazbanov <vkazbanov@telematika.com>
2025-02-10 20:44:07 +0200

    support extended BlockStem properties

--- src/api/java/powercrystals/minefactoryreloaded/farmables/harvestables/HarvestableStemPlant.java
+++ src/api/java/powercrystals/minefactoryreloaded/farmables/harvestables/HarvestableStemPlant.java
@@ -1,7 +1,9 @@
 package powercrystals.minefactoryreloaded.farmables.harvestables;
 
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockStem;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
@@ -9,6 +11,9 @@ import net.minecraft.world.World;
 import powercrystals.minefactoryreloaded.api.plant.HarvestType;
 import powercrystals.minefactoryreloaded.api.util.IFactorySettings;
 import powercrystals.minefactoryreloaded.api.util.IFactorySettings.SettingNames;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HarvestableStemPlant extends HarvestableCropMeta {
 
@@ -31,8 +36,32 @@ public class HarvestableStemPlant extends HarvestableCropMeta {
 			BlockPos start = new BlockPos(settings.getVec(SettingNames.START_POSITION));
 			EnumFacing facing = EnumFacing.getFacingFromVector(pos.getX() - start.getX(), pos.getY() - start.getY(), pos.getZ() - start.getZ());
 			harvestState = world.getBlockState(start).getActualState(world, start);
-			return harvestState.getValue(BlockStem.FACING) == facing;
+			if (harvestState.getPropertyKeys().contains(BlockStem.FACING)) {
+				return harvestState.getValue(BlockStem.FACING) == facing;
+			} else {
+				ImmutableMap<IProperty<?>, Comparable<?>> properties = harvestState.getProperties();
+				Map<String, Boolean> named = new HashMap<>();
+				for (Map.Entry<IProperty<?>, Comparable<?>> e : properties.entrySet()) {
+					if (e.getValue() instanceof Boolean) {
+						named.put(e.getKey().getName(), (Boolean) e.getValue());
+					}
+				}
+				switch (facing) {
+					case DOWN:
+					case UP:
+						return false;
+					case NORTH:
+						return named.getOrDefault("north", false);
+					case SOUTH:
+						return named.getOrDefault("south", false);
+					case WEST:
+						return named.getOrDefault("west", false);
+					case EAST:
+						return named.getOrDefault("east", false);
+				}
+			}
 		}
+		return false;
 	}
 
 }
